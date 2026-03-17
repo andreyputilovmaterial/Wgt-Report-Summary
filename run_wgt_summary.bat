845278e38ae9c68fdaee3e07a2afc6f10b1348d7
Andrey Putilov <Andrey.Putilov@materialplus.io>
2026-03-17 14:46:14 -0700

    Check for ErrorLevel in BAT file

--- run_wgt_summary.bat
+++ run_wgt_summary.bat
@@ -1,5 +1,7 @@
+@ECHO OFF
 
 
-python run_wgt_summary.py --file "Weight_Report_Assignment.htm"
+python run_wgt_summary.py --file "Weight_Report.htm"
+if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && exit /b %errorlevel% )
 
 
